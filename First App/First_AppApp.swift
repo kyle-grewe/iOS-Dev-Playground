eb1e0bcccf184e749ba35b40e0fb5067b22e2a73
Kyle <kgrewe@outlook.com>
2022-11-16 18:10:43 -0600

    Initial Commit

---
+++ First App/First_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_AppApp.swift
+//  First App
+//
+//  Created by Kyle Grewe on 11/16/22.
+//
+
+import SwiftUI
+
+@main
+struct First_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
